8c5f1ae15ec595e67a265fd64c2c1af59058fab0
nivred <emaildervin@gmail.com>
2017-12-16 20:00:36 -0600

    set up server code

--- server.js
+++ server.js
@@ -2,3 +2,24 @@ var bodyParser = require("body-parser");
 var methodOverride = require('method-override')
 var express = require("express")
 var app = express();
+
+// Choose a port number
+var PORT = process.env.PORT || 3000;
+// Use  express.static middleware to serve static content for app from "public" directory
+app.use(express.static("public"));
+// Set up express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+// Set Handlebars as default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller");
+
+app.use("/", routes);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
